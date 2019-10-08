68b6fdcc475e0a3a134e9cd78d990bc4395024d2
Lucas L. Soto <ls@adhoc.com.ar>
2019-10-07 21:30:20 -0300

    [IMP] helpdesk_subticket: add filters and groupings (#161)
    - Add filters "Is ticket" and "Is Sub-Ticket".
    - Add groupby "Parent Ticket".

--- helpdesk_subticket/__manifest__.py
+++ helpdesk_subticket/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Helpdesk Subticket',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.1.0',
     'category': 'Projects & Services',
     'sequence': 14,
     'summary': '',
