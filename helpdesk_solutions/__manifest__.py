8745147ffbfa95ecc0785e79ee5cbf72961eaa04
Nicolas Mac Rouillon <nmr@adhoc.com.ar>
2020-02-19 20:46:01 +0000

    [MIG] helpdesk_solutions: Migration to 13.0

--- helpdesk_solutions/__manifest__.py
+++ helpdesk_solutions/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Helpdesk Solutions',
-    'version': '12.0.1.1.0',
+    'version': "13.0.1.0.0",
     'category': 'Projects & Services',
     'sequence': 14,
     'summary': '',
@@ -41,7 +41,7 @@
     ],
     'demo': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
