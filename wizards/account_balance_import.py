168796925d3035a851ac37cad213bdbb22fddf8d
Daniel Libonati <dl@adhoc.com.ar>
2020-04-14 09:49:41 +0000

    [FIX] Multiple fixes

--- wizards/account_balance_import.py
+++ wizards/account_balance_import.py
@@ -299,43 +299,65 @@ class AccountBalanceImport(models.TransientModel):
                         str(row_no + 1)))
                 continue
 
-            # Skip if we're not able to parse date
-            try:
-                accounting_date = datetime.datetime(
-                    *xlrd.xldate_as_tuple(
-                        dict_data["date"],
-                        workbook.datemode)).date()
-            except TypeError:
-                errors.append(
-                    "Fila {}: Formato de fecha desconocido. "
-                    "Asegúrese de que la columna posee "
-                    "formato de fecha.".format(
-                        str(row_no + 1)))
-                continue
+            if dict_data["date"] != "":
+                try:
+                    accounting_date = datetime.datetime(
+                        *xlrd.xldate_as_tuple(
+                            dict_data["date"],
+                            workbook.datemode)).date()
+                except TypeError:
+                    # Skip if we're not able to parse date
+                    errors.append(
+                        "Fila {}: Formato de fecha desconocido. "
+                        "Asegúrese de que la columna posee "
+                        "formato de fecha.".format(
+                            str(row_no + 1)))
+                    continue
+            else:
+                # Default to accounting date if omitted
+                accounting_date = self.accounting_date
 
-            # Skip if we're not able to parse due_date
-            try:
-                due_date = datetime.datetime(
-                    *xlrd.xldate_as_tuple(
-                        dict_data["due_date"],
-                        workbook.datemode)).date()
-            except TypeError:
-                errors.append(
-                    "Fila {}: Formato de fecha de vencimiento desconocido. "
-                    "Asegúrese de que la columna posee "
-                    "formato de fecha.".format(
-                        str(row_no + 1)))
-                continue
+            if dict_data["due_date"] != "":
+                try:
+                    due_date = datetime.datetime(
+                        *xlrd.xldate_as_tuple(
+                            dict_data["due_date"],
+                            workbook.datemode)).date()
+                except TypeError:
+                    # Skip if we're not able to parse due_date
+                    errors.append(
+                        "Fila {}: Formato de fecha de vencimiento desconocido. "
+                        "Asegúrese de que la columna posee "
+                        "formato de fecha.".format(
+                            str(row_no + 1)))
+                    continue
+            else:
+                # Default to blank if ommited
+                due_date = False
 
             # Get account depending on selected type
             if self.partner_balance_type == "receivable":
+                # Receivable
                 partner_account = partner.property_account_receivable_id
-                debit = self.company_id.currency_id.round(dict_data["amount"])
-                credit = 0.0
-            else:  # Payable
+                if dict_data["amount"] > 0:
+                    debit = self.company_id.currency_id.round(dict_data["amount"])
+                    credit = 0.0
+                elif dict_data["amount"] < 0:
+                    debit = 0.0
+                    credit = self.company_id.currency_id.round(abs(dict_data["amount"]))
+                else:
+                    continue    # Skip silently if amount is 0
+            else:
+                # Payable
                 partner_account = partner.property_account_payable_id
-                debit = 0.0
-                credit = self.company_id.currency_id.round(dict_data["amount"])
+                if dict_data["amount"] > 0:
+                    debit = 0.0
+                    credit = self.company_id.currency_id.round(dict_data["amount"])
+                elif dict_data["amount"] < 0:
+                    debit = self.company_id.currency_id.round(abs(dict_data["amount"]))
+                    credit = 0.0
+                else:
+                    continue    # Skip silently if amount is 0
 
             # Skip if partner account account is obsolete
             if partner_account.deprecated:
@@ -400,7 +422,7 @@ class AccountBalanceImport(models.TransientModel):
             generated_move_ids.append(account_move.id)
 
         return {
-            "name": _("Imported Account Moves"),
+            "name": "Importación de Saldos Iniciales",
             "type": "ir.actions.act_window",
             "view_type": "form",
             "view_mode": "tree,form",
@@ -428,12 +450,23 @@ class AccountBalanceImport(models.TransientModel):
         ]
 
         generated_move_ids = list()   # For storing generated account move ids
+        pre_data = list()        # For storing data before persisting to db
         errors = list()         # For storing possible errors
 
         # Parse XLS file
         decoded = base64.decodestring(self.check_file)
         workbook = xlrd.open_workbook(file_contents=decoded)
         sheet = workbook.sheet_by_index(0)
+
+        # Get Journal
+        if self.check_type == 'issue_check':
+            operation = "handed"
+            journal = self.check_journal_bank_id
+            account = self.company_id._get_check_account('deferred')
+        else:  # Third-party Check
+            operation = "holding"
+            journal = self.check_journal_third_id
+            account = journal.default_debit_account_id
 
         # Iterate over each sheet row
         for row_no in range(1, sheet.nrows):
@@ -493,17 +526,13 @@ class AccountBalanceImport(models.TransientModel):
                         str(row_no + 1)))
                 continue
 
-            # Get Journal
-            if self.check_type == 'issue_check':
-                operation = "handed"
-                journal = self.check_journal_bank_id
-                account = self.company_id._get_check_account('deferred')
-            else:  # Third-party Check
-                operation = "holding"
-                journal = self.check_journal_third_id
-                account = journal.default_debit_account_id
-
-            number = str(int(dict_data["number"]))
+            try:
+                number = str(int(dict_data["number"]))
+            except ValueError:
+                errors.append(
+                    "Fila {}: Número de cheque inválido. ".format(
+                        str(row_no + 1)))
+                continue
 
             account_move_line_1 = {
                 "name": "Cheque N°. {}".format(number),
@@ -521,7 +550,7 @@ class AccountBalanceImport(models.TransientModel):
                 "partner_id": partner.id
             }
 
-            account_move = self.env['account.move'].create({
+            move_data = {
                 "date": self.accounting_date,
                 "journal_id": journal.id,
                 "ref": "Cheque N°. {}".format(number),
@@ -529,38 +558,53 @@ class AccountBalanceImport(models.TransientModel):
                     (0, 0, account_move_line_1),
                     (0, 0, account_move_line_2)
                 ]
-            })
+            }
 
-            # Post Account Move
-            account_move.post()
-
-            # Create Check
-            self.env['account.check'].create({
+            check_data = {
                 "number": number,
                 "amount": dict_data["amount"],
                 "bank_id": journal.bank_id.id,
                 "type": self.check_type,
-                "name": "Balance Import - Check No. {}".format(number),
+                "name": "Saldo Inicial - Cheque Nº. {}".format(number),
                 "journal_id": journal.id,
                 "checkbook_id": self.check_checkbook_id.id,
                 "issue_date": issue_date,
                 "payment_date": payment_date,
                 "owner_name": dict_data["owner_name"],
                 "owner_vat": str(int(dict_data["owner_vat"])) if dict_data["owner_vat"] else None,
-                "operation_ids": [
-                    (0, 0, {
-                        "date": self.accounting_date,  # TODO: Is this correct?
-                        "operation": operation,
-                        "origin": "{},{}".format("account.move", account_move.id),
-                        "partner_id": partner.id
-                    })
-                ]
-            })
+            }
 
+            pre_data.append((move_data, check_data, partner.id))
+
+        # Check if there were errors when iterating over XLS rows
+        if len(errors) > 0:
+            raise ValidationError("\n".join(errors))
+
+        if len(pre_data) == 0:
+            raise ValidationError(
+                "El archivo importado no contiene movimientos.")
+
+        for move_data, check_data, partid in pre_data:
+            # Create and Post Account Move
+            account_move = self.env["account.move"].create(move_data)
+            account_move.post()
             generated_move_ids.append(account_move.id)
 
+            # Update check data, add move as operation
+            check_data["operation_ids"] = [
+                (0, 0, {
+                    "date": self.accounting_date,
+                    "operation": operation,
+                    "origin": "{},{}".format("account.move", account_move.id),
+                    "partner_id": partid
+                })
+            ]
+
+            # Create Check
+            self.env['account.check'].create(check_data)
+
         return {
-            "name": _("Imported Account Moves"),
+            "name": "Importación de Saldos Iniciales",
             "type": "ir.actions.act_window",
             "view_type": "form",
             "view_mode": "tree,form",
