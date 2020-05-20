1eb13d91d6c4412c3312c13aa22f4a32e9756b23
Katherine Zaoral <kz@adhoc.com.ar>
2020-05-20 13:26:39 -0300

    [REM] l10n_ar_edi_ux: remove moved module

--- l10n_ar_edi_ux/models/__init__.py
+++
@@ -1,6 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import l10n_ar_afipws_connection
-from . import res_partner
