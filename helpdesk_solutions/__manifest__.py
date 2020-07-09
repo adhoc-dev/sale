8f71c769a3f702948092a69cc1644730204c1754
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-07-08 23:05:47 -0300

    [FIX] helpdesk_solutions: portal view

--- helpdesk_solutions/__manifest__.py
+++ helpdesk_solutions/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Helpdesk Solutions',
-    'version': "13.0.1.0.0",
+    'version': "13.0.1.1.0",
     'category': 'Projects & Services',
     'sequence': 14,
     'summary': '',
