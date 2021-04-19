78839aa3a97b987b5bc673bdb0bede696d6007ba
Nicolás Mac Rouillon <nmr@adhoc.com.ar>
2021-04-19 15:35:22 -0300

    [13.0] [FIX] helpdesk_ux: Reflog this changes and add to see msj for portal users.

--- helpdesk_ux/models/helpdesk_ticket.py
+++ helpdesk_ux/models/helpdesk_ticket.py
@@ -18,7 +18,14 @@ class HelpdeskTicket(models.Model):
 
     def _track_template(self, changes):
         ticket = self[0]
+        # PATCH START: Remove this part after odoo fix the error ...
         res = super()._track_template(changes)
+        if 'stage_id' in changes and ticket.stage_id.template_id:
+            res['stage_id'] = (ticket.stage_id.template_id, {
+                'message_type': 'comment',
+                'auto_delete_message': True,
+                'email_layout_xmlid': 'mail.mail_notification_light'})
+        # PATCH END: until this line
         if 'stage_id' in res and ticket.kanban_state == 'blocked' and \
                 ticket.stage_id.template_id:
             res.pop('stage_id')
