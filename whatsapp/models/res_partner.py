a4a6c3e04a530db3a8c247053bb7d3375a4c0060
Bruno Zanotti <bz@adhoc.com.ar>
2021-09-06 15:22:37 -0300

    [FIX] whatsapp: check format number
    Whatsapp expects a number in international format to redirect properly,
    so for Argentina, we should add a '9' after the country code (+54).
    
    Also, we hide the button if the format is not correct.

--- whatsapp/models/res_partner.py
+++ whatsapp/models/res_partner.py
@@ -3,15 +3,33 @@
 # directory
 ##############################################################################
 
-from odoo import models
+from odoo import models, fields, api
+import re
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
+    whatsapp_number = fields.Char(compute="_number_format")
+
     def whatsapp_redirect(self):
-        number = self.env.context.get('number')
         return {
             'type': 'ir.actions.act_url',
-            'url': "https://api.whatsapp.com/send?phone=%s" % number,
+            'url': "https://api.whatsapp.com/send?phone=%s" % self.whatsapp_number,
         }
+
+    @api.depends('mobile')
+    def _number_format(self):
+        # Whatsapp expect international number format
+        # All phone numbers in Argentina (country code "54") should have a "9" between the country code and area code.
+        # The prefix "15" must be removed so the final number will have 13 digits total: +54 9 XXX XXX XXXX
+        if self.mobile:
+            match = re.match(r'(\+54)[ ]{0,1}(9){0,1}(.*)', self.mobile)
+            if match:
+                self.whatsapp_number = '+54 9' + match.group(3)
+                if not len(self.whatsapp_number.replace(' ', '').replace('-', '')) == 14:
+                    self.whatsapp_number = None
+            else:
+                self.whatsapp_number = self.mobile
+        else:
+            self.whatsapp_number = None
