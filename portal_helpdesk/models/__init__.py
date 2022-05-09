f2ffda34859add476594a424eb1ed56b510cd90f
Juan Jose Scarafia <jjs@adhoc.com.ar>
2022-05-09 07:13:43 -0300

    [REM] portal_helpdesk: deprecated Lo depreciamos porque no sera muy util, no tenemos un caso de uso práctico donde fuera necesario

--- portal_helpdesk/models/__init__.py
+++
@@ -1,7 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-
-from . import helpdesk_ticket
-from . import helpdesk_team
