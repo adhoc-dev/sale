0d068595c1ab346e42bad0f4f192a30e054d25ea
Ignacio Cainelli <ica@adhoc.com.ar>
2022-05-12 12:09:21 +0000

    [FIX] account_online_sync_ar: extends access right
    Se extendio el permiso para los usuarios pertenecientes
    a Contabilidad / Administrador puedan ejecutar la accion
    manual_refresh
    
    closes ingadhoc/enterprise-extensions#186
    
    X-original-commit: 4159073be4e3b2b8c4e2b8f3b91202a073274c55
    Signed-off-by: Katherine Zaoral <kz@adhoc.com.ar>

--- account_online_sync_ar/__manifest__.py
+++ account_online_sync_ar/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "Account Online Sync Argentina",
-    'version': '13.0.1.9.0',
+    'version': '13.0.1.10.0',
     'category': 'Accounting/Accounting',
     'sequence': 14,
     'author': 'ADHOC SA',
