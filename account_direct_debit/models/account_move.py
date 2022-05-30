81092ac85feedad9805d8a33248240529e730b1a
Juan Jose Scarafia <jjs@adhoc.com.ar>
2022-05-30 11:56:57 +0000

    [FIX] direct_debit: post multiple invoices
    X-original-commit: 0a014febc50b6ef52a48b6e92728ea84c84064f1
    Part-of: ingadhoc/enterprise-extensions#192

--- account_direct_debit/models/account_move.py
+++ account_direct_debit/models/account_move.py
@@ -35,7 +35,11 @@ class AccountMove(models.Model):
         for rec in self:
             # TODO remove on v15 key create_from_expense, needed on v13 for payment group to be created
             # This code is only executed if the mandate may be used (thanks to the previous UserError)
-            payment = self.env['account.payment'].with_context(reate_from_expense=True).create({
+            # clean active_ids and active_id on context because when posting multiple invoices at once
+            # this method is called one by one but active_ids has all the invoices and then default_get of payment will
+            # raise an error that not every invoice is posted
+            payment = self.env['account.payment'].with_context(
+                active_ids=False, active_id=False, create_from_expense=True).create({
                     'invoice_ids': [(4, rec.id, None)],
                     'journal_id': rec.direct_debit_mandate_id.journal_id.id,
                     'payment_method_id': rec.env.ref('account_direct_debit.payment_method_direct_debit').id,
