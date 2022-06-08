4cb506e0395c0659d66c407059c544ca34f337b8
Juan Jose Scarafia <jjs@adhoc.com.ar>
2022-06-08 18:44:19 +0000

    [MIG] website_sale_algolia_search: Migration to 15.0
    Part-of: ingadhoc/enterprise-extensions#187

--- website_sale_algolia_search/__manifest__.py
+++ website_sale_algolia_search/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Algolia Search on e-commerce',
-    'version': '13.0.1.0.0',
+    'version': "15.0.1.0.0",
     'category': 'Uncategorized',
     'author': 'ADHOC SA',
     'license': 'AGPL-3',
@@ -15,5 +15,5 @@
         'base_algolia_search',
         'website_sale',
     ],
-    'installable': False,
+    'installable': True,
 }
