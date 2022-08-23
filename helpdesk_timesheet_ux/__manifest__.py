ae39670f4474eda529f12ec2c27bd9e7f19686db
Bruno Zanotti <bz@adhoc.com.ar>
2022-08-23 14:52:18 -0300

    [IMP] helpdesk_timesheet_ux: remove unused view / field

--- helpdesk_timesheet_ux/__manifest__.py
+++ helpdesk_timesheet_ux/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Helpdesk Timesheet UX',
-    'version': "15.0.1.0.0",
+    'version': "15.0.1.1.0",
     'category': 'Projects & Services',
     'sequence': 14,
     'summary': '',
@@ -35,7 +35,6 @@
         'views/helpdesk_ticket_views.xml',
         'views/project_project_views.xml',
         'views/helpdesk_team_views.xml',
-        'views/project_task_views.xml',
         'views/project_portal_template.xml',
     ],
     'demo': [
