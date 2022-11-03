bac8092c0403ec07e24709f0fa2a0bc23f372268
Maximiliano Mezzavilla <mem@adhoc.com>
2022-11-03 17:24:21 +0000

    [MIG] whatsapp: Migration to 16.0
    closes ingadhoc/enterprise-extensions#265
    
    Signed-off-by: Bruno Zanotti <bz@adhoc.com.ar>

--- whatsapp/__manifest__.py
+++ whatsapp/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Whatsapp',
-    'version': "15.0.1.0.0",
+    'version': "16.0.1.0.0",
     'category': 'Tools',
     'sequence': 14,
     'summary': '',
@@ -32,7 +32,7 @@
     'data': [
         'views/res_partner_view.xml',
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
