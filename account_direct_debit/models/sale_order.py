6a9db6d9dff20cb70d65f4e6866ce69788029c06
Pablo Montenegro <pam@adhoc.com.ar>
2023-03-17 18:25:47 +0000

    [IMP] account_direct_debit: Auto agregar a facturas de suscripciones
    Ticket: 60039
    Se agrega contrato de débito automático a las facturas que se crean automaticamente al correr el cron de "Suscripciones > Crear facturas"
    
    closes ingadhoc/enterprise-extensions#324
    
    Signed-off-by: Katherine Zaoral <kz@adhoc.com.ar>

--- account_direct_debit/models/sale_order.py
+++ account_direct_debit/models/sale_order.py
@@ -4,19 +4,12 @@ from odoo import models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    def action_invoice_subscription(self):
-        """ Here is assigned to the invoice the direct debit mandate if the partner has one """
-        res = super().action_invoice_subscription()
-
-        if res.get('res_id'):
-            account_move = self.env['account.move'].browse(res.get('res_id'))
-        else:
-            account_move = self.env['account.move'].search(res.get('domain'))
-
-        moves = account_move.filtered(lambda x: x.move_type == 'out_invoice' and x.partner_id)
-        for out_invoice_move in moves:
+    def _create_recurring_invoice(self, automatic=False, batch_size=30):
+        """ Here is assigned to the invoice the direct debit mandate if the partner has one. This method is called if the invoice is created from scheduled action 'Sale Subscription: generate recurring invoices and payments' """
+        account_moves = super()._create_recurring_invoice(automatic=automatic, batch_size=batch_size)
+        for out_invoice_move in account_moves.filtered(lambda x: x.move_type == 'out_invoice' and x.partner_id):
             mandate = self.env['account.direct_debit.mandate'].search([
                 ('partner_id.commercial_partner_id', '=', out_invoice_move.partner_id.commercial_partner_id.id),
                 ('state', '=', 'active')], limit=1)
             out_invoice_move.direct_debit_mandate_id = mandate
-        return res
+        return account_moves
