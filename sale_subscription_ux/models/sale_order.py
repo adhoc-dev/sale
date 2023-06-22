34608983e531bbd12071e40d2d05e7acfcef4e11
jok-adhoc <jok@adhoc.com.ar>
2023-06-22 16:42:47 +0000

    [FIX]sale_subscription_ux: _compute_price_unit fix
    closes ingadhoc/enterprise-extensions#353
    
    Signed-off-by: matiasperalta1 <mnp@adhoc.com.ar>

--- sale_subscription_ux/models/sale_order.py
+++ sale_subscription_ux/models/sale_order.py
@@ -94,17 +94,18 @@ class SaleOrderLine(models.Model):
     def _compute_price_unit(self):
         if not self._context.get('action_update_subscription_prices'):
             super()._compute_price_unit()
-        for line in self:
-            if not line.product_uom or not line.product_id or not line.order_id.pricelist_id:
-                line.price_unit = 0.0
-            else:
-                price = line.with_company(line.company_id)._get_display_price()
-                line.price_unit = line.product_id._get_tax_included_unit_price(
-                    line.company_id,
-                    line.order_id.currency_id,
-                    line.order_id.date_order,
-                    'sale',
-                    fiscal_position=line.order_id.fiscal_position_id,
-                    product_price_unit=price,
-                    product_currency=line.currency_id
-                )
+        else:
+            for line in self:
+                if not line.product_uom or not line.product_id or not line.order_id.pricelist_id:
+                    line.price_unit = 0.0
+                else:
+                    price = line.with_company(line.company_id)._get_display_price()
+                    line.price_unit = line.product_id._get_tax_included_unit_price(
+                        line.company_id,
+                        line.order_id.currency_id,
+                        line.order_id.date_order,
+                        'sale',
+                        fiscal_position=line.order_id.fiscal_position_id,
+                        product_price_unit=price,
+                        product_currency=line.currency_id
+                    )
